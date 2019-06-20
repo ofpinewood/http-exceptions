7afb69aa69a223d84d1a0eb017bc268a9b7ed257
Peter van den Hout <peter@ofpinewood.com>
2019-06-20 17:48:58 +0200

    WIP

--- samples/Opw.HttpExceptions.AspNetCore.Sample/Startup.cs
+++ samples/Opw.HttpExceptions.AspNetCore.Sample/Startup.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
 
 namespace Opw.HttpExceptions.AspNetCore.Sample
 {
