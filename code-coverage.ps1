41908ecf57413afbb9c75b2b8e856c8d2352abce
Peter van den Hout <peter@ofpinewood.com>
2019-06-22 12:26:16 +0200

    remove ps script

--- code-coverage.ps1
+++
@@ -1,9 +0,0 @@
-ForEach ($folder in (Get-ChildItem -Path .\tests -Directory)) {
-    $project = Get-ChildItem -Path ($folder.FullName + "\*.csproj") | Select-Object -First 1
-
-    Write-Host $project
-
-    dotnet test $project --configuration Release /p:CollectCoverage=true /p:Include="[Opw.*]*"
-}
-
-Read-Host -Prompt "Press ENTER to exit"
